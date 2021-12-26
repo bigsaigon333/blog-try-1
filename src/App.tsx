af7deb1ea8640cb99eee8a6a84a2dfad1152eaba
bigsaigon333 <likepeppermint@gmail.com>
2021-12-26 22:03:43 +0900

    feat: App 컴포넌트를 구현한다

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import Header from "./components/Header/Header";
+import Post from "./components/Post/Post";
+
+const post = {
+  title: "super",
+  description:
+    "super 는 현재 객체의 프로토타입 객체를 가리키는 포인터이며, Object.getPrototypeOf(this)의 값과 동일하다.간결한 메서드 안에서 super 참조를 사용하여 프로토타입 객체 내 어떤 메서드든 호출할 수 있다. super 참조를 사용하여 프로",
+  date: new Date("2021.11.12"),
+  minToRead: 3,
+};
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Post {...post} />
+    </div>
+  );
+};
+
+export default App;
