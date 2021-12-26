77e2a8ef8ff17e330fef1d5c2875aa58998f8e25
bigsaigon333 <likepeppermint@gmail.com>
2021-12-26 22:04:25 +0900

    feat: <App/> 을 render한다

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,5 @@
 import ReactDom from "react-dom";
-import Post from "./components/Post/Post";
+import App from "./App";
 import "./styles/remedy.css";
 
-const post = {
-  title: "super",
-  description:
-    "super 는 현재 객체의 프로토타입 객체를 가리키는 포인터이며, Object.getPrototypeOf(this)의 값과 동일하다.간결한 메서드 안에서 super 참조를 사용하여 프로토타입 객체 내 어떤 메서드든 호출할 수 있다. super 참조를 사용하여 프로",
-  date: new Date("2021.11.12"),
-  minToRead: 3,
-};
-
-ReactDom.render(<Post {...post} />, document.getElementById("root"));
+ReactDom.render(<App />, document.getElementById("root"));
