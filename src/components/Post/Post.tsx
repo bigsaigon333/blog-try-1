cf3e106be9ac5a3b6ffc7981672f4222b62dfee3
bigsaigon333 <likepeppermint@gmail.com>
2021-12-27 15:19:53 +0900

    feat: md의 body만 Post로 렌더링한다

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,21 +1,24 @@
 import { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
 import { fetchMarkdown } from "../../utils/api";
+import splitMarkdown from "../../utils/splitMarkdown";
 
 interface PostProps {
   route: string;
 }
 
 const Post = ({ route }: PostProps) => {
-  const [content, setContent] = useState("");
+  const [markdown, setMarkdown] = useState("");
 
   useEffect(() => {
-    fetchMarkdown(route).then(setContent);
-  }, [route, setContent]);
+    fetchMarkdown(route)
+      .then(splitMarkdown)
+      .then(([, body]) => setMarkdown(body));
+  }, [route, setMarkdown]);
 
   return (
     <section>
-      <ReactMarkdown>{content}</ReactMarkdown>
+      <ReactMarkdown>{markdown}</ReactMarkdown>
     </section>
   );
 };
